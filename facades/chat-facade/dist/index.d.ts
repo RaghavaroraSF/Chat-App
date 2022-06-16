91cdabacd9d2a30abd12623dd9d527b5a49a08ab
RaghavaroraSF <97958393+RaghavaroraSF@users.noreply.github.com>
2022-06-17 00:39:05 +0530

    updated

---
+++ facades/chat-facade/dist/index.d.ts
@@ -0,0 +1,3 @@
+import { ApplicationConfig, ChatfacadeApplication } from './application';
+export * from './application';
+export declare function main(options?: ApplicationConfig): Promise<ChatfacadeApplication>;
---
+++ facades/chat-facade/dist/permission-key.enum.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PermissionKey = void 0;
+var PermissionKey;
+(function (PermissionKey) {
+    PermissionKey["ViewMessage"] = "ViewMessage";
+    PermissionKey["CreateMessage"] = "CreateMessage";
+    PermissionKey["UpdateMessage"] = "UpdateMessage";
+    PermissionKey["DeleteMessage"] = "DeleteMessage";
+    PermissionKey["CreateMessageRecipient"] = "CreateMessageRecipient";
+    PermissionKey["ViewMessageRecipient"] = "ViewMessageRecipient";
+    PermissionKey["UpdateMessageRecipient"] = "UpdateMessageRecipient";
+    PermissionKey["DeleteMessageRecipient"] = "DeleteMessageRecipient";
+    PermissionKey["ViewNotification"] = "ViewNotification";
+    PermissionKey["CreateNotification"] = "CreateNotification";
+    PermissionKey["UpdateNotification"] = "UpdateNotification";
+    PermissionKey["DeleteNotification"] = "DeleteNotification";
+    PermissionKey["CanGetNotificationAccess"] = "CanGetNotificationAccess";
+    PermissionKey["ViewChannel"] = "ViewChannel";
+    PermissionKey["CreateChannel"] = "CreateChannel";
+    PermissionKey["UpdateChannel"] = "UpdateChannel";
+    PermissionKey["DeleteChannel"] = "DeleteChannel";
+})(PermissionKey = exports.PermissionKey || (exports.PermissionKey = {}));
+//# sourceMappingURL=permission-key.enum.js.map
