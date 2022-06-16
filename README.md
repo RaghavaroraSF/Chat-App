c3c76ccf6bf84a1bef512e0fde3aa220ae8e9d5e
Raghav <Raghav.arora@sourcefuse.com>
2022-06-16 23:40:08 +0530

    init change

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Chat-App
+A realtime chat application
--- facades/chat-facade/src/services/notif.service.ts
+++ facades/chat-facade/src/services/notif.service.ts
@@ -1,16 +1,28 @@
-import { inject, Provider, ValueOrPromise } from "@loopback/context";
-import { getService } from "@loopback/service-proxy";
-import { NotificationDataSource } from "../datasources/notification.datasource";
-type NotificationServiceProxy={
-    getNotification(token: string): Promise<void>;
-    postNotification(token:string,body:any):Promise<void>;
+import {inject, Provider} from '@loopback/core';
+import {getService} from '@loopback/service-proxy';
+import {NotificationDataSource} from '../datasources/notification.datasource';
+import {Pubnubnotification} from '../models/pubnubnotification';
+
+export interface Notificationservice {
+  // this is where you define the Node.js methods that will be
+  // mapped to REST/SOAP/gRPC operations as stated in the datasource
+  // json file.
+  getNotification(token: string): Promise<Pubnubnotification[]>;
+  createNotification(
+    data: Pubnubnotification,
+    token: string,
+  ): Promise<Pubnubnotification>;
 }
-export class NotificationServiceProvider implements Provider<NotificationServiceProxy>{
-    constructor(
-@inject('datasources.notification')
-protected dataSource:NotificationDataSource
-    ){}
-    value(): ValueOrPromise<NotificationServiceProxy> {
-        return getService(this.dataSource);
-    }
-}
+
+export class NotificationserviceProvider
+  implements Provider<Notificationservice>
+{
+  constructor(
+    // notification must match the name property in the datasource json file
+    @inject('datasources.notification')
+    protected dataSource: NotificationDataSource = new NotificationDataSource(),
+  ) {}
+
+  value(): Promise<Notificationservice> {
+    return getService(this.dataSource);
+  }}
