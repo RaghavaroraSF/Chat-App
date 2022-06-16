66d2cec305ada1302c4e27bcd81499ff5d7e4780
RaghavaroraSF <97958393+RaghavaroraSF@users.noreply.github.com>
2022-06-17 00:18:28 +0530

    updated

---
+++ services/authentication-service/dist/_test_/test-helper.d.ts
@@ -0,0 +1,7 @@
+import { ChatAppApplication } from '../..';
+import { Client } from '@loopback/testlab';
+export declare function setupApplication(): Promise<AppWithClient>;
+export interface AppWithClient {
+    app: ChatAppApplication;
+    client: Client;
+}
